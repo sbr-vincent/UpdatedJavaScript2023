de8496960e4d151556c2885512194d59c0d08611
sbr-vincent <vincent.g.sbr@gmail.com>
2023-09-12 08:54:57 -0700

    Completed Conditional Arrows

---
+++ conditionalArrowsCore/script.js
@@ -0,0 +1,19 @@
+// Problem 1
+const checkAge = age => age >= 18 ? "You are good to go!" : "Sorry! You must be 18 or older!"
+
+// Problem 2
+const isRaining = weather => weather ? "Get your rain jacket!" : "No rain on today's forecast!"
+
+// Problem 3
+const isEven = number => number % 2 === 0 ? "That's an even number!" : "That's an odd number!"
+
+// Problem 4
+const greaterNum = (num1, num2) => num1 > num2 ? `${num1} is greater than ${num2}` : `${num1} is less than ${num2}`
+
+
+
+
+
+
+
+
